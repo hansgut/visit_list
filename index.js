621cfd97aa7aea3e69f33365e60a1f0cd8f17389
Yan <yan@astarium.pl>
2020-04-25 17:50:09 +0200

    add auth

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const passport = require("passport");
 
+// Init the app
 const app = express();
 
-mongoose.connect("mongodb://localhost/visits",
-    { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
-app.use("/api", require("./api"));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
+require("./config/passport")(passport);
+// app.use("/api", require("./routes/api/api"));
 
-app.listen(4000, ()=>{
-    console.log("server is listening");
+// DB config
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log(`DB connected ${db}`);
+    }).catch(err => {
+        console.log(`Can't connect to DB ${err}`);
+    });
+
+app.get('/', (req, res) => {
+   return res.send("<h1>Hello</h1>")
+});
+
+const users = require('./routes/api/users');
+app.use('/api/users', users);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`server started on port ${PORT}`);
 })
---
+++ routes/api/users.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const key = require("../../config/keys").secret;
+const User = require("../../models/User");
+
+/**
+ * @route POST api/users/register
+ * @desc Register the User
+ * @access Public
+ */
+router.post('/register', (req, res) => {
+    let { name, surname, email, password, confirm_password } = req.body;
+    console.log(req.body);
+    console.log(password);
+    console.log(confirm_password);
+    if (password !== confirm_password){
+        return res.status(400).json({
+           msg: "Password do not match."
+        });
+    }
+
+    User.findOne({ email: email }).then(user => {
+       if(user){
+           return res.status(400).json({
+               msg: "Email is already registered."
+           })
+       }
+    });
+
+    let newUser = new User({
+        name,
+        surname,
+        email,
+        password
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+       bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if(err) throw err;
+          newUser.password = hash;
+          newUser.save().then(user => {
+             return res.status(201).json({
+                success: true,
+                 msg: "User is now registered."
+             });
+          });
+       });
+    });
+});
+
+/**
+ * @route POST api/users/login
+ * @desc Sign in the User
+ * @access Public
+ */
+router.post('/login', (req, res) => {
+   User.findOne({ email: req.body.email }).then(user => {
+       if(!user){
+           return res.status(404).json({
+               msg: "Email is not found.",
+               success: false
+           });
+       }
+
+      bcrypt.compare(req.body.password, user.password).then(isMatch => {
+         if(isMatch){
+             const payload = {
+                 _id: user.id,
+                 name: user.name,
+                 surname: user.surname,
+                 email: user.email
+             }
+             jwt.sign(payload, key, {
+                 expiresIn: 604800
+             }, (err, token) => {
+                res.status(200).json({
+                    success: true,
+                    token: `Bearer ${token}`,
+                    msg: "You are logged in."
+                });
+             });
+         } else {
+             return res.status(404).json({
+                msg: "Incorrect password.",
+                success: false
+             });
+         }
+      });
+   });
+});
+
+/**
+ * @route GET api/users/profile
+ * @desc Return the Users date
+ * @access Public
+ */
+router.get('/profile', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    return res.json({
+        user: req.user
+    });
+});
+
+
+
+module.exports = router;
+
+/*
+* {
+	"name": "Jan",
+	"surname": "Nowak",
+	"email": "jan.n@email.com",
+	"password": "123123123",
+	"confirm_password": "123123123"
+}
+* */
