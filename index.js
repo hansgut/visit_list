8a7236f8966ea9a6ee377d26dd42f0e3977337c5
Yan <yan@astarium.pl>
2020-04-26 06:27:37 +0200

    fix

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const users = require('./routes/api/users');
 app.use('/api/users', users);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
